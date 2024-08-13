5ddca7241c5da08efa7be648312a5aafd9a09f9d
Liamlunkamba <liamlunkamba12@gmail.com>
2024-08-13 14:54:59 -0400

    We are commiting

---
+++ src/main/java/rttdemo/Appsringdemo/AppsringdemoApplication.java
@@ -0,0 +1,13 @@
+package rttdemo.Appsringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppsringdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppsringdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rttdemo/Appsringdemo/AppsringdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package rttdemo.Appsringdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppsringdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
